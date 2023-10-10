26eb9f81e6056fac3ba6fcf0750996726825cb94
donalSE <143010113+donalSE@users.noreply.github.com>
2023-10-10 11:22:08 +0100

    Segmentation model changed

--- training/segmentation.py
+++ training/segmentation.py
@@ -2,12 +2,13 @@ from ultralytics import YOLO
 from roboflow import Roboflow
 from IPython import display
 display.clear_output()
+import time
 
 
 rf = Roboflow(api_key="hfGn1GDEH5TgyhZuTgiG")
 project = rf.workspace("donals-thesis").project("football-id-2")
 dataset = project.version(6).download("yolov8")
-
+time.sleep(15)
 
 # Load model
 model = YOLO('yolov8x-seg.pt')
